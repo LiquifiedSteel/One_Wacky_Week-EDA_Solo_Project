1872aaec03ae1bf35c025a961b10758790a4b4c1
Alex Rivard <alex.rivard4406@gmail.com>
2024-10-18 15:54:48 -0500

    payments are succesfully stored in the database

--- server/routes/payments.router.js
+++ server/routes/payments.router.js
@@ -22,7 +22,7 @@ router.post('/construct-session', rejectUnauthenticated, async (req, res) => {
         ],
         mode: 'payment',
         success_url: `${YOUR_DOMAIN}/#/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${YOUR_DOMAIN}/#/cancel`,
+        cancel_url: `${YOUR_DOMAIN}/#/download`,
         customer_email: email,
     });
 
@@ -34,5 +34,61 @@ router.post('/construct-session', rejectUnauthenticated, async (req, res) => {
         res.sendStatus(500);
     };
 });
+
+
+router.post('/verify', async (req, res) => {
+    const user = req.body.user;
+    const sessionId = req.body.sessionID;
+    console.log(user);
+    try {
+      // Retrieve the session from Stripe
+      const session = await stripe.checkout.sessions.retrieve(sessionId);
+      console.log('Retrieved session:', session);
+      if (session.payment_status === 'paid') {
+        const {
+          id: session_id,
+          payment_intent: paymentIntentId,
+          amount_total,
+          currency,
+          customer_email,
+        } = session;
+        // Retrieve quantity from the session object
+        const quantity = session.amount_total / 999; // Assuming 999 cents per game purchase
+        console.log('Session details - Email:', customer_email);
+        // Check if the payment has already been recorded
+        const paymentCheckQuery =
+          'SELECT "id" FROM "Payments" WHERE "user_email"=$1;';
+        const paymentCheckResult = await pool.query(paymentCheckQuery, [user.user_email]);
+        if (paymentCheckResult.rows.length === 0) {
+          const userQuery = 'SELECT id FROM "user" WHERE user_email = $1';
+          const userResult = await pool.query(userQuery, [customer_email]);
+          if (userResult.rows.length > 0) {
+            const userId = userResult.rows[0].id;
+            const queryText = `INSERT INTO "Payments" (user_email, amount) VALUES ($1, $2) RETURNING id;`;
+            // Set status as true for completed payments
+            const paymentResult = await pool.query(queryText, [
+              user.user_email,
+              amount_total / 100, // Convert from cents to dollars
+            ]);
+
+            console.log('Payment information stored with email:', paymentResult.rows[0].id);
+            res.json({ success: true });
+          } else {
+            console.error('User not found with email:', customer_email);
+            res.json({ success: false, error: 'User not found' });
+          }
+        } else {
+          console.log('Payment already recorded.');
+          res.json({ success: true, message: 'Payment already recorded.' });
+        }
+      } else {
+        res.json({ success: false, error: 'Payment not completed' });
+      }
+    } catch (error) {
+      console.error('Error verifying payment:', error);
+      res.status(500).json({ success: false, error: error.message });
+    }
+  });
+
 
 module.exports = router;
--- src/components/success/Success.jsx
+++ src/components/success/Success.jsx
@@ -1,11 +1,30 @@
-
+import { useEffect } from "react"
+import axios from "axios";
+import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 function Success() {
-
+    const user = useSelector((store) => store.user);
+    const location = useLocation();
+    const sessionID = new URLSearchParams(location.search).get('session_id');
+    const history = useHistory();
+    console.log(sessionID);
+    useEffect (() => {
+        axios({
+            method: 'POST',
+            url: '/api/payments/verify',
+            data: {sessionID: sessionID, user: user},
+          })
+          .then((response) => {
+            history.push('/download');
+          })
+          .catch(err => console.error('Failed to verify payment', err))
+    }, [])
 
     return (
         <div>
-            <p>Hello World!</p>
+            
         </div>
     )
 }
