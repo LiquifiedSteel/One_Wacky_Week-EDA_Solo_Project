860028ff8ec8bc62b529644781f51fb2889da8dd
Alex Rivard <alex.rivard4406@gmail.com>
2024-10-21 13:29:17 -0500

    when user returns from stripe, they will need to login again, this way the conditional rendering on the download page will work as intended

--- src/components/success/Success.jsx
+++ src/components/success/Success.jsx
@@ -1,12 +1,13 @@
 import { useEffect } from "react"
 import axios from "axios";
 import { useLocation } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 function Success() {
     const user = useSelector((store) => store.user);
     const location = useLocation();
+    const dispatch = useDispatch()
     const sessionID = new URLSearchParams(location.search).get('session_id');
     const history = useHistory();
     useEffect (() => {
@@ -16,6 +17,7 @@ function Success() {
             data: {sessionID: sessionID, user: user},
           })
           .then((response) => {
+            dispatch({type: "LOGOUT"});
             history.push('/download');
           })
           .catch(err => console.error('Failed to verify payment', err))
