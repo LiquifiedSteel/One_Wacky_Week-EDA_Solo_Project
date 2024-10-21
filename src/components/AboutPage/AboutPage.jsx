7fa090b75f1767f7450dc0f8bd6ebd52a3a6f991
Alex Rivard <alex.rivard4406@gmail.com>
2024-10-21 14:18:29 -0500

    about page updated

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -15,11 +15,11 @@ function AboutPage() {
 
           <div>
             <h3>About the Creator</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p>Hello, I am the creator of One Wacky Week. I will not be sharing my name just yet. However, I will share a little about myself. I am born and raised in the United States, somewhere in the Midwest, (will not disclose where), and have been a gamer since I was a little boy. My dad got me into League of Legends as my first video game, but as of recently decided I am sick of competetive video games. I prefer a good story, like Resident Evil, or Minecraft Story Mode. I know that MSM is a bit cringe but I loved it as a kid regardless. It was my inspiration to start this project. Although, I am a solo developer and am attempting a very ambitious project for my first game, so it will take me forever to get this game out. I am also a huge fan of Game Theory and the other Theorist channels on youtube. So my goal with this project is to make a game that all the channels might be able to collab on. I want to put elements of story into the minigames (Game Theory), original media (Film Theory), the food if possible (Food Theory), the clothing (Style Theory), and even have some easter eggs for GTLive in the form of a Multiverse of Matness easter egg, kudos ahead of time to whoever finds it. Feel free to join the Discord Channel where I will try to keep updates coming out for the progress of the game. Thanks a ton for reading all this! I guess I was just kind of rambling. Possibly just to use as many letters as possible for an easter egg you can look for. Possibly. Maybe... BYE!</p>
           </div>
           <div>
             <h3>About the Game</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p>One Wacky Week will be a story choice based g<em>am</em>e. As you play through the story there w<em>i</em>ll be choices, minigames, and easter eggs along the way. Each choice will influence the ending you get, so you will want to play through multiple tiems to get different endings. My desire is to have at least 60 different endings, some <em>good</em> and some bad, some will be slightly different from others, and others will be vastly different. With <em>enough</em> endings, you may be able to put together the larger story that is present.</p>
           </div>
 
         </div>
