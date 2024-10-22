ea9d13d2897a0b0c57661a028a12ad710ef5d0c4
Alex Rivard <alex.rivard4406@gmail.com>
2024-10-22 16:18:04 -0500

    Navbar behaves as intended

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,50 +5,51 @@ import './Nav.css';
 import { useSelector } from 'react-redux';
 
 import Image from 'react-bootstrap/Image';
+import { Navbar, Nav } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-function Nav() {
+function Navi() {
   const user = useSelector((store) => store.user);
 
   return (
-    <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">One Wacky Week</h2>
-      </Link>
-      <div className='navLinks'>
-        {user.isAdmin && <Link className="navLink" to="/admin">Admin</Link>}
-        <Link className="navLink" to="/home">
-          Home
+      <Navbar collapseOnSelect expand="lg" variant="dark" className="nav">
+        <Link to="/home">
+          <h2 className="nav-title">One Wacky Week</h2>
         </Link>
-        <Link className="navLink" to="/about">
-          About
-        </Link>
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        )}
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" className='d-lg-none' />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto navLinks">
+              {user.isAdmin && <Link className="navLink" href="/admin">Admin</Link>}
+              <Link className="navLink" to="/home">Home</Link>
+              <Link className="navLink" to="/about">About</Link>
+                      
+              {/* If no user is logged in, show these links */}
+              {!user.id && (
+                // If there's no user, show login/registration links
+                <Link className="navLink" to="/login">
+                  Login / Register
+                </Link>
+              )}
 
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            <Link className="navLink" to="/download">
-              Download
-            </Link>
+              {/* If a user is logged in, show these links */}
+              {user.id && (
+                <>
+                  <Link className="navLink" to="/download">
+                    Download
+                  </Link>
 
-            <LogOutButton className="navLink" />
+                  <LogOutButton className="navLink" />
 
-            <Link className='navLink account' to="/user">
-              {user.username}
-              <Image className='profilePic' src={user.image_url} />
-            </Link>
-          </>
-        )}
-      </div>
-    </div>
+                  <Link className='navLink account' to="/user">
+                    {user.username}
+                    <Image className='profilePic' src={user.image_url} />
+                  </Link>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+      </Navbar>
   );
 }
 
-export default Nav;
+export default Navi;
