d30c2ee2ded91a297b70b69adde429fabc8f4837
Alex Rivard <alex.rivard4406@gmail.com>
2024-10-24 14:55:04 -0500

    touch ups to landing and download pages

--- src/components/DownloadPage/DownloadPage.jsx
+++ src/components/DownloadPage/DownloadPage.jsx
@@ -34,10 +34,17 @@ function DownloadPage() {
       <Row>
         <Col xs={12} md={6} className='downloadCol'>
           <Card className='downloadCard'>
-
-              <button className='downloadButton btn mybtn' onClick={() => handleClick()}>{user.purchased ? "Download Windows" : "Purchase One Wacky Week"}</button>
+              {!user.purchased && <button className='downloadButton btn mybtn'>Purchase One Wacky Week</button>}
               
-              {user.purchased && <button className='downloadButton btn mybtn' onClick={() => handleClick()}>Download MacOS</button>}
+              {user.purchased && <button className='downloadButton btn mybtn' onClick={() => handleClick()}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-download" viewBox="0 0 16 16">
+                  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+                  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+                </svg> Download Windows</button>}
+
+              {user.purchased && <button className='downloadButton btn mybtn' onClick={() => handleClick()}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-download" viewBox="0 0 16 16">
+                  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+                  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+                </svg> Download MacOS</button>}
             
             {!user.purchased && <p className='downloadInstructions'>You need to click the "Purchase One Wacky Week" button to buy the game.</p>}
             {user.purchased && <p className='downloadInstructions'>You can click either button to recieve the download for the game. (both buttons currently give an alert confirming that you have purchased the game)</p>}
